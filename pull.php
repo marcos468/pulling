2c1d85c271633bcc339d2b724788e6b9e750bb8b
test-nsp <59933820+test-nsp@users.noreply.github.com>
2020-01-18 22:32:44 -0300

    actualizando el primer archivo .php

---
+++ pull.php
@@ -0,0 +1,13 @@
+<?php 
+
+ $persona = [
+               "nombre" => "Pedro",
+               "apellido" => "Sánchez",
+               "edad" => 50
+          ];
+
+
+          foreach ($persona as $valor) {
+               echo "$valor <br>";
+          }
+?>
